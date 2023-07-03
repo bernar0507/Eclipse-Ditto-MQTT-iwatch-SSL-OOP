c8a107de6b8e40dd183ac524bbced5a806c4c3d7
Bernardo Augusto <46348014+bernar0507@users.noreply.github.com>
2023-07-03 11:42:08 +0100

    Update send_data_iwatch.py

--- iwatch/send_data_iwatch.py
+++ iwatch/send_data_iwatch.py
@@ -26,9 +26,9 @@ class MQTTPublisher:
         self.client.on_disconnect = self.on_disconnect
         self.client.on_publish = self.on_publish
         self.client.on_message = self.on_message
-        self.client.tls_set(ca_certs="/app/Eclipse-Ditto-MQTT-iWatch-SSL/mosquitto/config/ca.crt",
-                            certfile="/app/Eclipse-Ditto-MQTT-iWatch-SSL/mosquitto/config/client.crt",
-                            keyfile="/app/Eclipse-Ditto-MQTT-iWatch-SSL/mosquitto/config/client.pk8",
+        self.client.tls_set(ca_certs="/app/Eclipse-Ditto-MQTT-iWatch-SSL-OOP/mosquitto/config/ca.crt",
+                            certfile="/app/Eclipse-Ditto-MQTT-iWatch-SSL-OOP/mosquitto/config/client.crt",
+                            keyfile="/app/Eclipse-Ditto-MQTT-iWatch-SSL-OOP/mosquitto/config/client.pk8",
                             tls_version=ssl.PROTOCOL_TLSv1_2)
         self.client.username_pw_set(self.username, self.password)
 
